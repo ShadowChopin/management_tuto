147f00069a00fc3c9c7dc35b0c8283fcb759e7cb
ShadowChopin <mintheking@naver.com>
2024-01-24 14:39:30 +0900

    add get customers REST API module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Customer from "./components/Customer";
-import React from "react";
+import React, { Component } from "react";
 import {
   Table,
   TableBody,
@@ -31,68 +31,88 @@ const StyledTable = styled(Table)({
   minWidth: 1080,
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://picsum.photos/id/237/64/64",
-    name: "홍길동",
-    birthday: "961222",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 2,
-    image: "https://picsum.photos/seed/picsum/64/64",
-    name: "쿨뷰티",
-    birthday: "951019",
-    gender: "여자",
-    job: "대학생",
-  },
-  {
-    id: 3,
-    image: "https://picsum.photos/64/64?grayscale",
-    name: "갤럭시",
-    birthday: "980405",
-    gender: "남자",
-    job: "대학생",
-  },
-];
+// const customers = [
+//   {
+//     id: 1,
+//     image: "https://picsum.photos/id/237/64/64",
+//     name: "홍길동",
+//     birthday: "961222",
+//     gender: "남자",
+//     job: "대학생",
+//   },
+//   {
+//     id: 2,
+//     image: "https://picsum.photos/seed/picsum/64/64",
+//     name: "쿨뷰티",
+//     birthday: "951019",
+//     gender: "여자",
+//     job: "대학생",
+//   },
+//   {
+//     id: 3,
+//     image: "https://picsum.photos/64/64?grayscale",
+//     name: "갤럭시",
+//     birthday: "980405",
+//     gender: "남자",
+//     job: "대학생",
+//   },
+// ];
 
-function App(props) {
-  const { classes } = props;
-  return (
-    <StyledPaper>
-      <StyledTable className={classes && classes.root}>
-        <Table className={classes && classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>직업</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id} // map을 이용하여 다수의 데이터를 가져올때는 사용해야함
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
-          </TableBody>
-        </Table>
-      </StyledTable>
-    </StyledPaper>
-  );
+class App extends Component {
+  state = {
+    customers: "",
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <StyledPaper>
+        <StyledTable className={classes && classes.root}>
+          <Table className={classes && classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.customers
+                ? this.state.customers.map((c) => {
+                    return (
+                      <Customer
+                        key={c.id}
+                        id={c.id}
+                        image={c.image}
+                        name={c.name}
+                        birthday={c.birthday}
+                        gender={c.gender}
+                        job={c.job}
+                      />
+                    );
+                  })
+                : ""}
+            </TableBody>
+          </Table>
+        </StyledTable>
+      </StyledPaper>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// package.json / npm 사용할때?
+// "dev": "concurrently --kill-others-on-fail \"npm run server\" \"npm run client\""
+
+//npm run dev
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,8 +11,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://picsum.photos/id/237/64/64",
+      name: "홍길동",
+      birthday: "961222",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: 2,
+      image: "https://picsum.photos/seed/picsum/64/64",
+      name: "쿨뷰티",
+      birthday: "951019",
+      gender: "여자",
+      job: "대학생",
+    },
+    {
+      id: 3,
+      image: "https://picsum.photos/64/64?grayscale",
+      name: "갤럭시",
+      birthday: "980405",
+      gender: "남자",
+      job: "대학생",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
